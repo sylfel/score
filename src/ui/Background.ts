35ec9233f870dc665fc136d923e742caf20ecf55
Jean-Philippe THEVENOUX <jp.thevenoux@pm.me>
2023-04-24 07:35:13 +0200

    style: change background
    add some "stars" to background

--- src/ui/Background.ts
+++ src/ui/Background.ts
@@ -1,31 +1,64 @@
-import { Container, Sprite, Texture } from 'pixi.js'
+import { Color, Container, Graphics } from 'pixi.js'
 import { app } from '../main'
 
+const nbStars = 100
+const maxSpeed = 3
+
+interface Star {
+  speed: number
+  graphic: Graphics
+}
+
 export class Background extends Container {
-  private image: Sprite
-  private speed: number
+  private stars: Star[]
+  private innerHeight: number
+  private innerWidth: number
 
   constructor() {
     super()
+    this.innerHeight = 0
+    this.innerWidth = 0
+    this.stars = new Array(nbStars) as Star[]
+    for (let i = 0; i < nbStars; i++) {
+      const speed =
+        maxSpeed - Math.floor(Math.log(Math.random() * Math.exp(maxSpeed) + 1))
+      console.log(speed)
+      const graphic = new Graphics()
+      graphic.beginFill(
+        new Color({ h: 0, s: 0, l: (100 / maxSpeed) * speed }).toHex(),
+      )
+      graphic.drawRect(0, 0, speed * 2, speed * 2)
+      graphic.endFill()
 
-    this.image = Sprite.from(Texture.from('clampy'))
-    this.image.anchor.set(0.5)
-    this.image.angle = Math.random() * 360
-    this.speed = Math.random() * 2 - 1
+      this.stars[i] = {
+        speed,
+        graphic,
+      }
 
-    this.addChild(this.image)
+      this.addChild(graphic)
+    }
   }
 
   public resize(width: number, height: number) {
-    const ratio = width / this.image.texture.width
-    this.image.scale.set(Math.min(ratio / 2, 1))
-    this.image.position.x = width * 0.5
-    this.image.position.y = height * 0.5
+    this.innerHeight = height
+    this.innerWidth = width
+
+    for (let i = 0; i < nbStars; i++) {
+      this.stars[i].graphic.x = Math.random() * width
+      this.stars[i].graphic.y = Math.random() * height
+    }
   }
 
   updateTransform(): void {
     super.updateTransform()
     const delta = app.ticker.deltaTime
-    this.image.angle += delta * this.speed
+    for (let i = 0; i < nbStars; i++) {
+      const g = this.stars[i].graphic
+      g.y = g.y + Math.floor(this.stars[i].speed * delta)
+      if (g.y > this.innerHeight) {
+        g.y = 0
+        g.x = Math.random() * this.innerWidth
+      }
+    }
   }
 }
